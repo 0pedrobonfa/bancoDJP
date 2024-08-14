73eb94b74a6da11624e470afe2e5e5d0b3dcbd61
Pedro Bonfa <pedro.bonfa@estudante.ufjf.br>
2024-08-13 22:21:27 -0300

    construtor básico

---
+++ classe.js
@@ -0,0 +1,20 @@
+class contaBancaria
+{
+    constructor(nome, saldo){
+
+        this.nome = nome;
+        this.saldo = saldo;
+
+    }
+    
+    deposita(quantidade)
+    {
+        this.saldo = this.saldo + quantidade;
+        console.log("Você depositou", quantidade);
+        console.log("Seu saldo agora é", this.saldo);
+    }
+
+}
+
+const pessoa1 = new contaBancaria("Diego M.", 6000);
+pessoa1.deposita(100);
