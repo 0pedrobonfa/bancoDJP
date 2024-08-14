dc95c2e3190110221d8dbe46b02a1cb4a77aa1c7
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2024-08-14 14:36:06 -0300

    js está funcionando como deveria

--- classe.js
+++ classe.js
@@ -1,33 +1,32 @@
-class contaBancaria
-{
-    constructor(nome, saldo){
-
+class ContaBancaria {
+    constructor(nome, saldo) {
         this.nome = nome;
         this.saldo = saldo;
         this.registro = [];
-
     }
-    deposita(quantidade)
-    {
-        this.saldo = this.saldo + quantidade;
+
+    deposita(quantidade) {
+        this.saldo += quantidade;
         console.log("Você depositou", quantidade);
         console.log(`Seu saldo agora é ${this.saldo}`);
-        this.saldo.adicionaHistorico(quantidade);
+        this.registro.push(`Depositou ${quantidade}`);
     }
-    saca(quantidade)
-    {
-        this.saldo = this.saldo - quantidade;
+
+    saca(quantidade) {
+        this.saldo -= quantidade;
         console.log("Você sacou", quantidade);
         console.log(`Seu saldo agora é ${this.saldo}`);
+        this.registro.push(`Sacou ${quantidade}`);
     }
-    mostraHistorico()
-    {
-        //for
-    }
-    
 
+    mostraHistorico() {
+        for (let i = 0; i < this.registro.length; i++) {
+            console.log(this.registro[i]);
+        }
+    }
 }
 
-const pessoa1 = new contaBancaria("Diego M.", 6000);
+const pessoa1 = new ContaBancaria("Diego M.", 6000);
 pessoa1.deposita(100);
 pessoa1.saca(500);
+pessoa1.mostraHistorico();
