5304108160741cd3c18a5a89306006af2de983f8
Pedro Bonfa <pedro.bonfa@estudante.ufjf.br>
2024-08-13 22:25:26 -0300

    métodos estão funcionais

--- classe.js
+++ classe.js
@@ -6,15 +6,22 @@ class contaBancaria
         this.saldo = saldo;
 
     }
-    
     deposita(quantidade)
     {
         this.saldo = this.saldo + quantidade;
         console.log("Você depositou", quantidade);
-        console.log("Seu saldo agora é", this.saldo);
+        console.log(`Seu saldo agora é ${this.saldo}`);
     }
+    saca(quantidade)
+    {
+        this.saldo = this.saldo - quantidade;
+        console.log("Você sacou", quantidade);
+        console.log(`Seu saldo agora é ${this.saldo}`);
+    }
+    
 
 }
 
 const pessoa1 = new contaBancaria("Diego M.", 6000);
 pessoa1.deposita(100);
+pessoa1.saca(500);
